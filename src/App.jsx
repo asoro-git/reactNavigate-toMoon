5fb770b4a4c90365bca35e08cafcce0de028c047
study s <ohvelynuna@gmail.com>
2025-01-05 08:03:08 +1100

    path

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,8 @@ function Explore() {
 	const navigate = useNavigate();
 	return (
 		<>
+			<b>Your location: {location.pathname}</b>
+			<br />
 			<h2>Explore Moon!!!</h2>
 			<button onClick={() => navigate("/", { state: { from: "Moon" } })}>
 				Back to Earth
@@ -26,8 +28,10 @@ function Explore() {
 
 function Counter() {
 	const [counter, setCounter] = useState(0);
+	const location = useLocation();
 	return (
 		<>
+			<br />
 			<p>You have clicked {counter} times.</p>
 			{counter >= 9 ? (
 				<p>Get ready! We are heading to the Moooon!!!</p>
@@ -35,7 +39,7 @@ function Counter() {
 			<button onClick={() => setCounter((prev) => prev + 1)}>
 				Click me!
 			</button>
-			{counter > 10 ? <Navigate to="./Explore" /> : null}
+			{counter > 10 ? <Navigate to="/explore" /> : null}
 		</>
 	);
 }
@@ -44,10 +48,16 @@ function Home() {
 	const location = useLocation();
 	const from = location.state?.from;
 	if (!from) {
-		return <h1>Welcome to Earth!</h1>;
+		return (
+			<>
+				<b>Your location: {location.pathname}</b>
+				<h1>Welcome to Earth!</h1>
+			</>
+		);
 	}
 	return (
 		<>
+			<b>Your location: {location.pathname}</b>
 			<h1>Returned from {from} !</h1>
 			<Counter />
 		</>
@@ -56,12 +66,12 @@ function Home() {
 
 function App() {
 	const [count, setCount] = useState(0);
-
 	return (
 		<Router>
 			<Link to="/">Home Page</Link>
 			{" | "}
 			<Link to="./explore">Explore</Link>
+			<br />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/explore" element={<Explore />} />
